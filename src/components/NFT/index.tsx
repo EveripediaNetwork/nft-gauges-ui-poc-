5f8df45be2db824f165364e476536bc9318f69b7
JustinCast <justincastillovalladares@gmail.com>
2022-07-20 11:44:08 -0600

    wip: add brainy abi & mint

--- src/components/NFT/index.tsx
+++ src/components/NFT/index.tsx
@@ -1,10 +1,24 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useContractWrite, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
+import { utils } from "ethers"
+
+import { BrainyAbi } from '../../abis/brainy.abi'
 
 import styles from '../../styles/Home.module.css'
 
 const NFT = () => {
   const { address, isConnected } = useAccount()
+  const { writeAsync: mint } = useContractWrite({
+    addressOrName: "0x196c92baa8af0a685bee4a87fe35e570c2215b36",
+    chainId: 5,
+    contractInterface: BrainyAbi,
+    functionName: "publicMint",
+    args: [1],
+    overrides: {
+      value: utils.parseEther("0.03"),
+    }
+  })
+
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   })
@@ -18,7 +32,7 @@ const NFT = () => {
       </h1>
       <div className={styles.buttonContainer}>
         <button onClick={() => isConnected ? disconnect() : connect()}>{isConnected ? "Disconnect" : "Connect"} wallet</button>
-        <button onClick={() => connect()}>Mint new nft</button>
+        <button onClick={() => mint()}>Mint new nft</button>
       </div>
 
       <div className={styles.grid}>
